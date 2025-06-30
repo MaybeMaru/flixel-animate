e01cc6fce88bdd93b386f6f38633077bb7af40df
DM-kun <86197998+DMMaster636@users.noreply.github.com>
2025-06-30 16:08:02 +0200

    fix anim FlxAnimateController override

--- src/animate/FlxAnimate.hx
+++ src/animate/FlxAnimate.hx
@@ -16,8 +16,15 @@ class FlxAnimate extends FlxSprite
 {
 	public static var drawDebugLimbs:Bool = false;
 
+	public var anim(default, set):FlxAnimateController = null;
+	private function set_anim(newController:FlxAnimateController):FlxAnimateController
+	{
+		anim = newController;
+		animation = anim;
+		return newController;
+	}
+
 	public var library(default, null):FlxAnimateFrames;
-	public var anim(default, null):FlxAnimateController;
 	public var isAnimate(default, null):Bool = false;
 	public var timeline:Timeline;
 
