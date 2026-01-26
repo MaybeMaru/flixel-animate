4846142f5ab16fae568618a3ee51f40f84bc54dd
MaybeMaru <97055307+MaybeMaru@users.noreply.github.com>
2026-01-26 02:40:32 +0100

    last one of these i swear

--- src/animate/FlxAnimate.hx
+++ src/animate/FlxAnimate.hx
@@ -270,7 +270,6 @@ class FlxAnimate extends FlxSprite
 		if (doStageMatrix)
 		{
 			matrix.translate(timeline._bounds.x, timeline._bounds.y);
-			matrix.concat(library.matrix);
 		}
 
 		matrix.translate(-origin.x, -origin.y);
@@ -286,6 +285,11 @@ class FlxAnimate extends FlxSprite
 		{
 			updateSkew();
 			matrix.concat(_skewMatrix);
+		}
+
+		if (doStageMatrix) // TODO: add some way to customize the order of this thing
+		{
+			matrix.concat(library.matrix);
 		}
 
 		getScreenPosition(_point, camera);
